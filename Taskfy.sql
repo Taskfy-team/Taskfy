93c235ceb2e3219b58101fffdfacb02619b6df6a
OtavioSantanna <otavio.santana085@gmail.com>
2025-05-25 17:53:10 -0300

    alterações no banco dados

--- Taskfy.sql
+++ Taskfy.sql
@@ -1,229 +1,350 @@
--- MariaDB dump 10.19  Distrib 10.4.32-MariaDB, for Win64 (AMD64)
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
 --
--- Host: localhost    Database: taskfy
--- ------------------------------------------------------
--- Server version	10.4.32-MariaDB
+-- Host: 127.0.0.1
+-- Tempo de geração: 25/05/2025 às 22:52
+-- Versão do servidor: 10.4.32-MariaDB
+-- Versão do PHP: 8.2.12
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
 /*!40101 SET NAMES utf8mb4 */;
-/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
-/*!40103 SET TIME_ZONE='+00:00' */;
-/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
-/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
-/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
-/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
 --
--- Table structure for table `admin`
+-- Banco de dados: `taskfy`
 --
 
-DROP TABLE IF EXISTS `admin`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
+-- --------------------------------------------------------
+
+--
+-- Estrutura para tabela `admin`
+--
+
 CREATE TABLE `admin` (
-  `id_admin` int(11) NOT NULL AUTO_INCREMENT,
+  `id_admin` int(11) NOT NULL,
   `nome_admin` varchar(30) DEFAULT NULL,
   `email_admin` varchar(30) DEFAULT NULL,
-  `senha_admin` varchar(30) DEFAULT NULL,
-  PRIMARY KEY (`id_admin`)
+  `senha_admin` varchar(30) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-/*!40101 SET character_set_client = @saved_cs_client */;
 
 --
--- Dumping data for table `admin`
+-- Despejando dados para a tabela `admin`
 --
 
-LOCK TABLES `admin` WRITE;
-/*!40000 ALTER TABLE `admin` DISABLE KEYS */;
-/*!40000 ALTER TABLE `admin` ENABLE KEYS */;
-UNLOCK TABLES;
+INSERT INTO `admin` (`id_admin`, `nome_admin`, `email_admin`, `senha_admin`) VALUES
+(1, 'otavio', 'otavio@email.com', '123'),
+(2, 'gustavo', 'gustavo@gmail.com', '123');
+
+-- --------------------------------------------------------
 
 --
--- Table structure for table `chat`
+-- Estrutura para tabela `chat`
 --
 
-DROP TABLE IF EXISTS `chat`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `chat` (
-  `id_chat` int(11) NOT NULL AUTO_INCREMENT,
+  `id_chat` int(11) NOT NULL,
   `mensagem_chat` text DEFAULT NULL,
   `data_mensagem` date DEFAULT NULL,
   `fk_equipe` int(11) DEFAULT NULL,
   `fk_tarefa` int(11) DEFAULT NULL,
-  `fk_usuario` int(11) DEFAULT NULL,
-  PRIMARY KEY (`id_chat`),
-  KEY `fk_equipe_chat` (`fk_equipe`),
-  KEY `fk_tarefa_chat` (`fk_tarefa`),
-  KEY `fk_usuario_chat` (`fk_usuario`),
-  CONSTRAINT `fk_equipe_chat` FOREIGN KEY (`fk_equipe`) REFERENCES `equipes` (`id_equipe`),
-  CONSTRAINT `fk_tarefa_chat` FOREIGN KEY (`fk_tarefa`) REFERENCES `tarefas` (`id_tarefa`),
-  CONSTRAINT `fk_usuario_chat` FOREIGN KEY (`fk_usuario`) REFERENCES `usuario` (`id_usuario`)
+  `fk_usuario` int(11) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-/*!40101 SET character_set_client = @saved_cs_client */;
+
+-- --------------------------------------------------------
 
 --
--- Dumping data for table `chat`
+-- Estrutura para tabela `equipes`
 --
 
-LOCK TABLES `chat` WRITE;
-/*!40000 ALTER TABLE `chat` DISABLE KEYS */;
-/*!40000 ALTER TABLE `chat` ENABLE KEYS */;
-UNLOCK TABLES;
-
---
--- Table structure for table `equipes`
---
-
-DROP TABLE IF EXISTS `equipes`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `equipes` (
-  `id_equipe` int(11) NOT NULL AUTO_INCREMENT,
+  `id_equipe` int(11) NOT NULL,
   `nome_equipe` varchar(20) DEFAULT NULL,
   `desc_equipe` varchar(40) DEFAULT NULL,
   `status_equipe` varchar(10) DEFAULT NULL,
-  PRIMARY KEY (`id_equipe`)
+  `donoEquipe` int(11) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-/*!40101 SET character_set_client = @saved_cs_client */;
 
 --
--- Dumping data for table `equipes`
+-- Despejando dados para a tabela `equipes`
 --
 
-LOCK TABLES `equipes` WRITE;
-/*!40000 ALTER TABLE `equipes` DISABLE KEYS */;
-/*!40000 ALTER TABLE `equipes` ENABLE KEYS */;
-UNLOCK TABLES;
+INSERT INTO `equipes` (`id_equipe`, `nome_equipe`, `desc_equipe`, `status_equipe`, `donoEquipe`) VALUES
+(1, 'equipe alfa', 'equipe de teste alfa', 'ativo', 1),
+(2, 'equipe testes', 'segunda equipe para testes', 'ativo', 2);
+
+-- --------------------------------------------------------
 
 --
--- Table structure for table `tarefas`
+-- Estrutura para tabela `tarefas`
 --
 
-DROP TABLE IF EXISTS `tarefas`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `tarefas` (
-  `id_tarefa` int(11) NOT NULL AUTO_INCREMENT,
+  `id_tarefa` int(11) NOT NULL,
   `nome_tarefa` varchar(20) DEFAULT NULL,
   `descricao_tarefa` varchar(40) DEFAULT NULL,
   `status_tarefa` varchar(10) DEFAULT NULL,
   `fk_dono_tarefa` int(11) DEFAULT NULL,
   `fk_equipe` int(11) DEFAULT NULL,
-  PRIMARY KEY (`id_tarefa`),
-  KEY `fk_dono_tarefa` (`fk_dono_tarefa`),
-  KEY `fk_equipe` (`fk_equipe`),
-  CONSTRAINT `fk_dono_tarefa` FOREIGN KEY (`fk_dono_tarefa`) REFERENCES `usuario` (`id_usuario`),
-  CONSTRAINT `fk_equipe` FOREIGN KEY (`fk_equipe`) REFERENCES `equipes` (`id_equipe`)
+  `dataCriacao` date NOT NULL,
+  `prazoTarefa` date NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-/*!40101 SET character_set_client = @saved_cs_client */;
 
 --
--- Dumping data for table `tarefas`
+-- Despejando dados para a tabela `tarefas`
 --
 
-LOCK TABLES `tarefas` WRITE;
-/*!40000 ALTER TABLE `tarefas` DISABLE KEYS */;
-/*!40000 ALTER TABLE `tarefas` ENABLE KEYS */;
-UNLOCK TABLES;
+INSERT INTO `tarefas` (`id_tarefa`, `nome_tarefa`, `descricao_tarefa`, `status_tarefa`, `fk_dono_tarefa`, `fk_equipe`, `dataCriacao`, `prazoTarefa`) VALUES
+(1, 'tarefa de teste', 'tarefa criada para teste', 'ativa', 1, 1, '2025-05-06', '2025-08-07');
+
+-- --------------------------------------------------------
 
 --
--- Table structure for table `timeline_tarefa`
+-- Estrutura para tabela `timeline_tarefa`
 --
 
-DROP TABLE IF EXISTS `timeline_tarefa`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `timeline_tarefa` (
-  `id_timeline` int(11) NOT NULL AUTO_INCREMENT,
+  `id_timeline` int(11) NOT NULL,
   `mensagem_timeline` text DEFAULT NULL,
   `data_timline` date DEFAULT NULL,
   `anexo_timeline` mediumblob DEFAULT NULL,
   `fk_equipe` int(11) DEFAULT NULL,
   `fk_tarefa` int(11) DEFAULT NULL,
-  `fk_usuario` int(11) DEFAULT NULL,
-  PRIMARY KEY (`id_timeline`),
-  KEY `fk_equipe_timeline` (`fk_equipe`),
-  KEY `fk_tarefa_timeline` (`fk_tarefa`),
-  KEY `fk_usuario_timeline` (`fk_usuario`),
-  CONSTRAINT `fk_equipe_timeline` FOREIGN KEY (`fk_equipe`) REFERENCES `equipes` (`id_equipe`),
-  CONSTRAINT `fk_tarefa_timeline` FOREIGN KEY (`fk_tarefa`) REFERENCES `tarefas` (`id_tarefa`),
-  CONSTRAINT `fk_usuario_timeline` FOREIGN KEY (`fk_usuario`) REFERENCES `usuario` (`id_usuario`)
+  `fk_usuario` int(11) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-/*!40101 SET character_set_client = @saved_cs_client */;
+
+-- --------------------------------------------------------
 
 --
--- Dumping data for table `timeline_tarefa`
+-- Estrutura para tabela `usuario`
 --
 
-LOCK TABLES `timeline_tarefa` WRITE;
-/*!40000 ALTER TABLE `timeline_tarefa` DISABLE KEYS */;
-/*!40000 ALTER TABLE `timeline_tarefa` ENABLE KEYS */;
-UNLOCK TABLES;
-
---
--- Table structure for table `usuario`
---
-
-DROP TABLE IF EXISTS `usuario`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `usuario` (
-  `id_usuario` int(11) NOT NULL AUTO_INCREMENT,
+  `id_usuario` int(11) NOT NULL,
   `nome_usuario` varchar(30) DEFAULT NULL,
   `email_usuario` varchar(30) DEFAULT NULL,
   `senha_usuario` varchar(30) DEFAULT NULL,
-  PRIMARY KEY (`id_usuario`)
-) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-/*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Dumping data for table `usuario`
---
-
-LOCK TABLES `usuario` WRITE;
-/*!40000 ALTER TABLE `usuario` DISABLE KEYS */;
-INSERT INTO `usuario` VALUES (1,'adolfo','minecraftalemao@ww2.bol.com','123'),(2,'carlao','cal@ao.com','123'),(3,'marcos','marc@os.com','123');
-/*!40000 ALTER TABLE `usuario` ENABLE KEYS */;
-UNLOCK TABLES;
-
---
--- Table structure for table `usuario_tarefa`
---
-
-DROP TABLE IF EXISTS `usuario_tarefa`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `usuario_tarefa` (
-  `id_use_taf` int(11) NOT NULL AUTO_INCREMENT,
-  `fk_usuario` int(11) NOT NULL,
-  `fk_tarefa` int(11) NOT NULL,
-  PRIMARY KEY (`id_use_taf`),
-  KEY `fk_usutaf_usuaio` (`fk_usuario`),
-  KEY `fk_usutaf_tarefa` (`fk_tarefa`),
-  CONSTRAINT `fk_usutaf_tarefa` FOREIGN KEY (`fk_tarefa`) REFERENCES `tarefas` (`id_tarefa`),
-  CONSTRAINT `fk_usutaf_usuaio` FOREIGN KEY (`fk_usuario`) REFERENCES `usuario` (`id_usuario`)
+  `dataNascimento` date DEFAULT NULL,
+  `numeroTelefone` varchar(11) NOT NULL,
+  `status` varchar(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-/*!40101 SET character_set_client = @saved_cs_client */;
 
 --
--- Dumping data for table `usuario_tarefa`
+-- Despejando dados para a tabela `usuario`
 --
 
-LOCK TABLES `usuario_tarefa` WRITE;
-/*!40000 ALTER TABLE `usuario_tarefa` DISABLE KEYS */;
-/*!40000 ALTER TABLE `usuario_tarefa` ENABLE KEYS */;
-UNLOCK TABLES;
-/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+INSERT INTO `usuario` (`id_usuario`, `nome_usuario`, `email_usuario`, `senha_usuario`, `dataNascimento`, `numeroTelefone`, `status`) VALUES
+(1, 'adolfo', 'minecraftalemao@ww2.bol.com', '123', NULL, '', ''),
+(2, 'carlao', 'cal@ao.com', '123', NULL, '', ''),
+(4, 'evandro', 'evandro@email.com', '123', '1985-05-09', '988445555', 'ativo'),
+(5, 'marcos', 'marcos@email.com', '123', '2006-06-22', '999675742', 'ativo');
 
-/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
-/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
-/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+-- --------------------------------------------------------
+
+--
+-- Estrutura para tabela `usuario_equipe`
+--
+
+CREATE TABLE `usuario_equipe` (
+  `id_usr_equipe` int(11) NOT NULL,
+  `fk_equipe` int(11) DEFAULT NULL,
+  `fk_usuario` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Despejando dados para a tabela `usuario_equipe`
+--
+
+INSERT INTO `usuario_equipe` (`id_usr_equipe`, `fk_equipe`, `fk_usuario`) VALUES
+(1, 1, 1),
+(2, 1, 2),
+(3, 1, 4),
+(4, 2, 5),
+(9, 2, 1);
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura para tabela `usuario_tarefa`
+--
+
+CREATE TABLE `usuario_tarefa` (
+  `id_use_taf` int(11) NOT NULL,
+  `fk_usuario` int(11) NOT NULL,
+  `fk_tarefa` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Índices para tabelas despejadas
+--
+
+--
+-- Índices de tabela `admin`
+--
+ALTER TABLE `admin`
+  ADD PRIMARY KEY (`id_admin`);
+
+--
+-- Índices de tabela `chat`
+--
+ALTER TABLE `chat`
+  ADD PRIMARY KEY (`id_chat`),
+  ADD KEY `fk_equipe_chat` (`fk_equipe`),
+  ADD KEY `fk_tarefa_chat` (`fk_tarefa`),
+  ADD KEY `fk_usuario_chat` (`fk_usuario`);
+
+--
+-- Índices de tabela `equipes`
+--
+ALTER TABLE `equipes`
+  ADD PRIMARY KEY (`id_equipe`),
+  ADD KEY `fk_donoEquipe` (`donoEquipe`);
+
+--
+-- Índices de tabela `tarefas`
+--
+ALTER TABLE `tarefas`
+  ADD PRIMARY KEY (`id_tarefa`),
+  ADD KEY `fk_dono_tarefa` (`fk_dono_tarefa`),
+  ADD KEY `fk_equipe` (`fk_equipe`);
+
+--
+-- Índices de tabela `timeline_tarefa`
+--
+ALTER TABLE `timeline_tarefa`
+  ADD PRIMARY KEY (`id_timeline`),
+  ADD KEY `fk_equipe_timeline` (`fk_equipe`),
+  ADD KEY `fk_tarefa_timeline` (`fk_tarefa`),
+  ADD KEY `fk_usuario_timeline` (`fk_usuario`);
+
+--
+-- Índices de tabela `usuario`
+--
+ALTER TABLE `usuario`
+  ADD PRIMARY KEY (`id_usuario`);
+
+--
+-- Índices de tabela `usuario_equipe`
+--
+ALTER TABLE `usuario_equipe`
+  ADD PRIMARY KEY (`id_usr_equipe`),
+  ADD KEY `fk_equipe` (`fk_equipe`),
+  ADD KEY `fk_usuario` (`fk_usuario`);
+
+--
+-- Índices de tabela `usuario_tarefa`
+--
+ALTER TABLE `usuario_tarefa`
+  ADD PRIMARY KEY (`id_use_taf`),
+  ADD KEY `fk_usutaf_usuaio` (`fk_usuario`),
+  ADD KEY `fk_usutaf_tarefa` (`fk_tarefa`);
+
+--
+-- AUTO_INCREMENT para tabelas despejadas
+--
+
+--
+-- AUTO_INCREMENT de tabela `admin`
+--
+ALTER TABLE `admin`
+  MODIFY `id_admin` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- AUTO_INCREMENT de tabela `chat`
+--
+ALTER TABLE `chat`
+  MODIFY `id_chat` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de tabela `equipes`
+--
+ALTER TABLE `equipes`
+  MODIFY `id_equipe` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- AUTO_INCREMENT de tabela `tarefas`
+--
+ALTER TABLE `tarefas`
+  MODIFY `id_tarefa` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT de tabela `timeline_tarefa`
+--
+ALTER TABLE `timeline_tarefa`
+  MODIFY `id_timeline` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de tabela `usuario`
+--
+ALTER TABLE `usuario`
+  MODIFY `id_usuario` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+
+--
+-- AUTO_INCREMENT de tabela `usuario_equipe`
+--
+ALTER TABLE `usuario_equipe`
+  MODIFY `id_usr_equipe` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
+
+--
+-- AUTO_INCREMENT de tabela `usuario_tarefa`
+--
+ALTER TABLE `usuario_tarefa`
+  MODIFY `id_use_taf` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- Restrições para tabelas despejadas
+--
+
+--
+-- Restrições para tabelas `chat`
+--
+ALTER TABLE `chat`
+  ADD CONSTRAINT `fk_equipe_chat` FOREIGN KEY (`fk_equipe`) REFERENCES `equipes` (`id_equipe`),
+  ADD CONSTRAINT `fk_tarefa_chat` FOREIGN KEY (`fk_tarefa`) REFERENCES `tarefas` (`id_tarefa`),
+  ADD CONSTRAINT `fk_usuario_chat` FOREIGN KEY (`fk_usuario`) REFERENCES `usuario` (`id_usuario`);
+
+--
+-- Restrições para tabelas `equipes`
+--
+ALTER TABLE `equipes`
+  ADD CONSTRAINT `fk_donoEquipe` FOREIGN KEY (`donoEquipe`) REFERENCES `usuario` (`id_usuario`);
+
+--
+-- Restrições para tabelas `tarefas`
+--
+ALTER TABLE `tarefas`
+  ADD CONSTRAINT `fk_dono_tarefa` FOREIGN KEY (`fk_dono_tarefa`) REFERENCES `usuario` (`id_usuario`),
+  ADD CONSTRAINT `fk_equipe` FOREIGN KEY (`fk_equipe`) REFERENCES `equipes` (`id_equipe`);
+
+--
+-- Restrições para tabelas `timeline_tarefa`
+--
+ALTER TABLE `timeline_tarefa`
+  ADD CONSTRAINT `fk_equipe_timeline` FOREIGN KEY (`fk_equipe`) REFERENCES `equipes` (`id_equipe`),
+  ADD CONSTRAINT `fk_tarefa_timeline` FOREIGN KEY (`fk_tarefa`) REFERENCES `tarefas` (`id_tarefa`),
+  ADD CONSTRAINT `fk_usuario_timeline` FOREIGN KEY (`fk_usuario`) REFERENCES `usuario` (`id_usuario`);
+
+--
+-- Restrições para tabelas `usuario_equipe`
+--
+ALTER TABLE `usuario_equipe`
+  ADD CONSTRAINT `usuario_equipe_ibfk_1` FOREIGN KEY (`fk_equipe`) REFERENCES `equipes` (`id_equipe`),
+  ADD CONSTRAINT `usuario_equipe_ibfk_2` FOREIGN KEY (`fk_usuario`) REFERENCES `usuario` (`id_usuario`);
+
+--
+-- Restrições para tabelas `usuario_tarefa`
+--
+ALTER TABLE `usuario_tarefa`
+  ADD CONSTRAINT `fk_usutaf_tarefa` FOREIGN KEY (`fk_tarefa`) REFERENCES `tarefas` (`id_tarefa`),
+  ADD CONSTRAINT `fk_usutaf_usuaio` FOREIGN KEY (`fk_usuario`) REFERENCES `usuario` (`id_usuario`);
+COMMIT;
+
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
-/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
-
--- Dump completed on 2025-04-09 21:56:54
