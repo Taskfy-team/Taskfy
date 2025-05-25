00fe6d8d0e675826e4915270ef63b18a064ccc54
0gudu <gugag3rr@gmail.com>
2025-05-25 02:38:01 -0300

    Criado paginas html

--- teste/views-objects/views-js/load-sideba.js
+++
@@ -1,7 +0,0 @@
-fetch('views-objects/html/navBar.html')
-  .then(res => res.text())
-  .then(html => {
-    document.getElementById('sidebar-placeholder').innerHTML = html;
-    console.log('Navbar carregada!');
-  })
-  .catch(err => console.error('Erro ao carregar navbar:', err));
